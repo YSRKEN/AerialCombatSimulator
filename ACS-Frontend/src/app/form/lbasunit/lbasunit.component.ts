b3cf46a8abe2532a5467f33aa977462980e14550
YSRKEN <ysr.ken@gmail.com>
2018-10-10 22:35:27 +0900

    基地航空隊の出撃回数を選択できるようにした

--- ACS-Frontend/src/app/form/lbasunit/lbasunit.component.ts
+++ ACS-Frontend/src/app/form/lbasunit/lbasunit.component.ts
@@ -9,6 +9,20 @@ import { SaveDataService } from '../../service/save-data.service';
 export class LBASUnitComponent implements OnInit {
 
   /**
+   * 基地航空隊の状態一覧
+   */
+  LBASCountList: { "value": string, "name": string }[] = [
+    {"value":"なし", "name":"なし"},
+    {"value":"分散", "name":"分散"},
+    {"value":"集中", "name":"集中"},
+  ];
+
+  /**
+   * 選択している状態
+   */
+  LBASCount: string;
+
+  /**
    * 装備一覧
    */
   WeaponList: { "value": string, "name": string }[] = [
@@ -39,10 +53,20 @@ export class LBASUnitComponent implements OnInit {
     /**
      * 設定を読み込む
      */
+    this.LBASCount = this.saveData.loadString('lbasunit.lbas_count' + this.index, 'なし');
     const weaponCount = 4;
     for(let i = 0; i < weaponCount; ++i){
       this.SelectedWeapon[i] = this.saveData.loadString('lbasunit.selected_weapon_' + this.index + '' + (i + 1), 'なし');
     }
+  }
+
+  /**
+   * 基地航空隊の状態を切り替えた際の処理
+   * @param event 
+   */
+  changeLBASCount(event: any){
+    this.LBASCount = event;
+    this.saveData.saveString('lbasunit.lbas_count' + this.index, this.LBASCount);
   }
 
   /**
