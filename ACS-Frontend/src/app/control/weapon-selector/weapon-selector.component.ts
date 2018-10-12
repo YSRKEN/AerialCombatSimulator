c44cdec885092279c0d194e53bc5a329287f2656
YSRKEN <ysr.ken@gmail.com>
2018-10-13 00:35:06 +0900

    装備種リストを表示するルーチンを書いた

--- ACS-Frontend/src/app/control/weapon-selector/weapon-selector.component.ts
+++ ACS-Frontend/src/app/control/weapon-selector/weapon-selector.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-weapon-selector',
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WeaponSelectorComponent implements OnInit {
 
+  /**
+   * 基地航空隊の装備だけ表示するならtrue
+   */
+  @Input() category: string;
+
+  WeaponTypeList: { "value": string, "name": string }[];
+
   constructor() { }
 
   ngOnInit() {
+    // 装備種リストを初期化
+    switch(this.category){
+    case 'LBAS':
+      this.WeaponTypeList = [
+        { "value": "0", "name": "なし" },
+        { "value": "5", "name": "艦戦" },
+        { "value": "6", "name": "艦爆" },
+        { "value": "7", "name": "艦攻" },
+        { "value": "8", "name": "艦偵" },
+      ];
+      break;
+    case 'Normal':
+      this.WeaponTypeList = [
+        { "value": "0", "name": "なし" },
+        { "value": "1", "name": "主砲" },
+        { "value": "2", "name": "副砲" },
+        { "value": "3", "name": "魚雷" },
+        { "value": "5", "name": "艦戦" },
+        { "value": "6", "name": "艦爆" },
+        { "value": "7", "name": "艦攻" },
+        { "value": "8", "name": "艦偵" },
+        { "value": "11", "name": "水偵" },
+      ];
+      break;
+    default:
+      this.WeaponTypeList = [
+        { "value": "0", "name": "なし" },
+      ];
+      break;
+    }
   }
-
 }
