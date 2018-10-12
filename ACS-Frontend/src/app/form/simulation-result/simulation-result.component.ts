6fc3f3759c769e163bb632b7c4ebe8d07d6b36c6
YSRKEN <ysr.ken@gmail.com>
2018-10-12 09:56:00 +0900

    シミュレーションボタンを作成

--- ACS-Frontend/src/app/form/simulation-result/simulation-result.component.ts
+++ ACS-Frontend/src/app/form/simulation-result/simulation-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SaveDataService } from '../../service/save-data.service';
 
 @Component({
   selector: 'app-simulation-result',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SimulationResultComponent implements OnInit {
 
-  constructor() { }
+  SimulationType: string = '1';
+
+  constructor(private saveData: SaveDataService) { }
 
   ngOnInit() {
+    this.SimulationType = this.saveData.loadString('simulation-result.simulation_type', '1');
   }
 
+  /**
+   * シミュレーションの種類を切り替えた際の処理
+   * @param event 
+   */
+  changeSimulationType(event: any) {
+    this.SimulationType = event;
+    this.saveData.saveString('simulation-result.simulation_type', this.SimulationType);
+  }
 }
