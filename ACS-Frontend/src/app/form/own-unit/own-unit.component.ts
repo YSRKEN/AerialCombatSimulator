d11569faec26639e79f0a4775cfa1cdbd7629e73
YSRKEN <ysr.ken@gmail.com>
2018-10-13 16:44:00 +0900

    搭載数および表示スロット数を、艦名によって切り替えできるようにした

--- ACS-Frontend/src/app/form/own-unit/own-unit.component.ts
+++ ACS-Frontend/src/app/form/own-unit/own-unit.component.ts
@@ -21,35 +21,70 @@ export class OwnUnitComponent implements OnInit {
 
   /**
    * 艦名リストを返す
+   * @param kammusuTypeValue 艦種
    */
-  private getKammusuNameList(type: string): { "value": string, "name": string }[] {
-    switch (type) {
+  private getKammusuNameList(kammusuTypeValue: string): { "value": string, "name": string, "slot": number[] }[] {
+    switch (kammusuTypeValue) {
     case '2':
       return [
-        { "value": "0", "name": "なし" },
-        { "value": "9", "name": "吹雪" },
-        { "value": "10", "name": "白雪" },
-        { "value": "11", "name": "深雪" },
-        { "value": "32", "name": "初雪" },
+        { "value": "0", "name": "なし", "slot": [] },
+        { "value": "9", "name": "吹雪", "slot": [0,0] },
+        { "value": "10", "name": "白雪", "slot": [0,0] },
+        { "value": "11", "name": "深雪", "slot": [0,0] },
+        { "value": "32", "name": "初雪", "slot": [0,0] },
       ];
     case '7':
       return [
-        { "value": "0", "name": "なし" },
-        { "value": "89", "name": "鳳翔" },
+        { "value": "0", "name": "なし", "slot": [] },
+        { "value": "89", "name": "鳳翔", "slot": [8,11] },
       ];
     case '11':
       return [
-        { "value": "0", "name": "なし" },
-        { "value": "196", "name": "飛龍改二" },
-        { "value": "197", "name": "蒼龍改二" },
-        { "value": "277", "name": "赤城改" },
-        { "value": "278", "name": "加賀改" },
+        { "value": "0", "name": "なし", "slot": [] },
+        { "value": "196", "name": "飛龍改二", "slot": [18,36,22,3] },
+        { "value": "197", "name": "蒼龍改二", "slot": [18,35,20,6] },
+        { "value": "277", "name": "赤城改", "slot": [20,20,32,10] },
+        { "value": "278", "name": "加賀改", "slot": [20,20,46,12] },
       ];
     default:
       return [
-        { "value": "0", "name": "なし" }
+        { "value": "0", "name": "なし", "slot": [] }
       ];
     }
+  }
+
+  /**
+   * 最大搭載数を返す
+   * @param kammusuNameValue 艦名
+   */
+  private getSlotSize(kammusuNameValue: string): string[] {
+    const slotSize = ['0', '0', '0', '0', '0'];
+
+    // 艦の情報を検索する
+    const list = this.KammusuNameList.filter(pair => pair.value === kammusuNameValue);
+    if (list.length === 0) {
+      return slotSize;
+    }
+    const kammusuSlotInfo = list[0].slot;
+
+    // 順次登録する
+    for(let i = 0; i < kammusuSlotInfo.length; ++i){
+      slotSize[i] = '' + kammusuSlotInfo[i];
+    }
+    return slotSize;
+  }
+
+  /**
+   * スロット数を返す
+   * @param kammusuNameValue 艦名
+   */
+  private getSlotCount(kammusuNameValue: string): number {
+    // 艦の情報を検索する
+    const list = this.KammusuNameList.filter(pair => pair.value === kammusuNameValue);
+    if (list.length === 0) {
+      return 0;
+    }
+    return list[0].slot.length;
   }
 
   /**
@@ -70,7 +105,17 @@ export class OwnUnitComponent implements OnInit {
   /**
    * 艦名一覧
    */
-  KammusuNameList: { "value": string, "name": string }[];
+  KammusuNameList: { "value": string, "name": string, "slot": number[] }[];
+
+  /**
+   * 最大搭載数
+   */
+  SlotSize: string[];
+
+  /**
+   * スロット数
+   */
+  SlotCount: number;
 
   /**
    * 艦種
@@ -96,7 +141,14 @@ export class OwnUnitComponent implements OnInit {
     return this.saveData.loadString('own-unit.[' + this.index + '].kammusu_name', '0');
   }
   set KammusuNameValue(value: string) {
+    // 保存
     this.saveData.saveString('own-unit.[' + this.index + '].kammusu_name', value);
+
+    // 搭載数リストを更新
+    this.SlotSize = this.getSlotSize(this.KammusuNameValue);
+
+    // スロット数を更新
+    this.SlotCount = this.getSlotCount(this.KammusuNameValue);
   }
 
   constructor(private saveData: SaveDataService) { }
@@ -107,6 +159,12 @@ export class OwnUnitComponent implements OnInit {
     
     // 艦名リストを初期化
     this.KammusuNameList = this.getKammusuNameList(this.KammusuTypeValue);
+
+    // 搭載数リストを更新
+    this.SlotSize = this.getSlotSize(this.KammusuNameValue);
+
+    // スロット数を更新
+    this.SlotCount = this.getSlotCount(this.KammusuNameValue);
   }
 
   /**
