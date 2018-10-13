fa6d93fa7c1d92ed1f99d27594fab656392711d5
YSRKEN <ysr.ken@gmail.com>
2018-10-13 10:27:08 +0900

    装備種の選択を保存できるようにした。また、コードの構造を変更した

--- ACS-Frontend/src/app/control/weapon-selector/weapon-selector.component.ts
+++ ACS-Frontend/src/app/control/weapon-selector/weapon-selector.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { SaveDataService } from '../../service/save-data.service';
 
 @Component({
   selector: 'app-weapon-selector',
@@ -8,44 +9,75 @@ import { Component, OnInit, Input } from '@angular/core';
 export class WeaponSelectorComponent implements OnInit {
 
   /**
-   * 基地航空隊の装備だけ表示するならtrue
+   * 装備種カテゴリ
+   * 'LBAS' => 基地航空隊の装備だけ表示する
+   * 'Normal' => 全ての装備を表示する
+   * その他 => どの装備も表示しない
    */
   @Input() category: string;
 
+  /**
+   * 保存時のキーに付ける接頭辞
+   */
+  @Input() prefix: string;
+
+  /**
+   * 装備種リスト
+   */
   WeaponTypeList: { "value": string, "name": string }[];
 
-  constructor() { }
+  /**
+   * 装備種に適合した装備種リストを返す
+   * @param category 装備種カテゴリ
+   */
+  private getWeaponTypeList(category: string): { "value": string, "name": string }[] {
+    switch(category){
+      case 'LBAS':
+        return [
+          { "value": "0", "name": "なし" },
+          { "value": "5", "name": "艦戦" },
+          { "value": "6", "name": "艦爆" },
+          { "value": "7", "name": "艦攻" },
+          { "value": "8", "name": "艦偵" },
+        ];
+      case 'Normal':
+        return [
+          { "value": "0", "name": "なし" },
+          { "value": "1", "name": "主砲" },
+          { "value": "2", "name": "副砲" },
+          { "value": "3", "name": "魚雷" },
+          { "value": "5", "name": "艦戦" },
+          { "value": "6", "name": "艦爆" },
+          { "value": "7", "name": "艦攻" },
+          { "value": "8", "name": "艦偵" },
+          { "value": "11", "name": "水偵" },
+        ];
+      default:
+        return [
+          { "value": "0", "name": "なし" },
+        ];
+      }
+  }
+
+  constructor(private saveData: SaveDataService) { }
 
   ngOnInit() {
-    // 装備種リストを初期化
-    switch(this.category){
-    case 'LBAS':
-      this.WeaponTypeList = [
-        { "value": "0", "name": "なし" },
-        { "value": "5", "name": "艦戦" },
-        { "value": "6", "name": "艦爆" },
-        { "value": "7", "name": "艦攻" },
-        { "value": "8", "name": "艦偵" },
-      ];
-      break;
-    case 'Normal':
-      this.WeaponTypeList = [
-        { "value": "0", "name": "なし" },
-        { "value": "1", "name": "主砲" },
-        { "value": "2", "name": "副砲" },
-        { "value": "3", "name": "魚雷" },
-        { "value": "5", "name": "艦戦" },
-        { "value": "6", "name": "艦爆" },
-        { "value": "7", "name": "艦攻" },
-        { "value": "8", "name": "艦偵" },
-        { "value": "11", "name": "水偵" },
-      ];
-      break;
-    default:
-      this.WeaponTypeList = [
-        { "value": "0", "name": "なし" },
-      ];
-      break;
+    // @Inputをチェックする
+    if (this.prefix === undefined || undefined === '') {
+      throw new Error("<app-weapon-selector>のprefix属性は省略できません。");
     }
+
+    // 装備種リストを初期化
+    this.WeaponTypeList = this.getWeaponTypeList(this.category);
+  }
+
+  /**
+   * 装備種
+   */
+  get WeaponType(): string {
+    return this.saveData.loadString(this.prefix + '.weapon_type', '0');
+  }
+  set WeaponType(value: string) {
+    this.saveData.saveString(this.prefix + '.weapon_type', value);
   }
 }
