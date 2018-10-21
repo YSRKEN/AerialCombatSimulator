369ccad9617cb26352ab05395ddbbfa1ee983c3e
YSRKEN <ysr.ken@gmail.com>
2018-10-21 15:39:31 +0900

    ・テーブルを自動作成するスクリプトを作成 ・フロントエンドからAPIを叩く際は、データをキャッシュして何度も叩かないように配慮

--- ACS-Frontend/src/app/service/rest-api.service.ts
+++ ACS-Frontend/src/app/service/rest-api.service.ts
@@ -11,9 +11,23 @@ export type ValueNamePair = { "value": string, "name": string };
 })
 export class RestApiService {
 
+  /**
+   * アクセスする先のサーバーアドレス
+   */
   private serverUrl = "http://localhost:8080";
 
+  /**
+   * データキャッシュ
+   */
+  private cache :{[key: string]: any; } = {};
+
+  /**
+   * セマフォ
+   */
+  private semaphore: boolean = false;
+
   constructor(private http: HttpClient) {
+    // ローカルサーバーで動いていない際は、本番用serverにアクセスするようにする
     if (window.location.href.indexOf("localhost") < 0){
       this.serverUrl = "https://aerial-combat-service.appspot.com";
     }
@@ -24,7 +38,27 @@ export class RestApiService {
    */
   public async getWeaponTypes(): Promise<ValueNamePair[]> {
     try {
-      return await this.http.get<ValueNamePair[]>(this.serverUrl + '/weapon-types').toPromise();
+      // キャッシュに存在する場合はそちらを返却する
+      if ("getWeaponTypes" in this.cache){
+        return this.cache["getWeaponTypes"];
+      }
+
+      // セマフォが立っている際は、何かしらの通信中なので、読み込みを待機する
+      if (this.semaphore) {
+        await new Promise(resolve => setTimeout(resolve, 100));
+        return this.getWeaponTypes();
+      }
+
+      // 寝ているセマフォを立て、通信後にまた寝かせる
+      // 
+      this.semaphore = true;
+      const result = await this.http.get<ValueNamePair[]>(this.serverUrl + '/weapon-types').toPromise();
+      this.semaphore = false;
+
+      // キャッシュにデータを追加する
+      this.cache["getWeaponTypes"] = result;
+
+      return result;
     } catch (e) {
       console.log(e);
       return [];
---
+++ ACS-Utility/create_db.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+import os
+import pandas
+import sqlite3
+from contextlib import closing
+
+def has_table(cursor, table_name: str) -> bool:
+    """{table_name}テーブルの存在を確認する。あればTrueを返す
+    """
+    command = f"SELECT COUNT(*) FROM sqlite_master WHERE TYPE='table' AND name='{table_name}'"
+    cursor.execute(command)
+    return cursor.fetchone()[0] != 0
+
+# 当該Pythonファイルのディレクトリ
+ROOT_DIRECTORY = os.path.dirname(__file__)
+
+# データベースファイルの相対ファイルパス
+DB_PATH = '../ACS-Server/src/main/webapp/WEB-INF/GameData_.db'
+
+with closing(sqlite3.connect(os.path.join(ROOT_DIRECTORY, DB_PATH))) as connect:
+    cursor = connect.cursor()
+
+    # 装備種テーブルを作成する
+    if has_table(cursor, 'weapon_type'):
+        cursor.execute('DROP TABLE weapon_type')
+    command = '''CREATE TABLE [weapon_type] (
+                 [id] INTEGER NOT NULL UNIQUE,
+                 [name] TEXT NOT NULL UNIQUE,
+                 [short_name] TEXT NOT NULL UNIQUE,
+                 PRIMARY KEY([id]))'''
+    cursor.execute(command)
+
+    # 装備種テーブルにデータを追加する
+    command = 'INSERT INTO weapon_type (id, name, short_name) VALUES (?,?,?)'
+    weapon_type_df = pandas.read_csv(os.path.join(ROOT_DIRECTORY, 'weapon_type.csv'))
+    data = list(map(lambda x: (x[0], x[1], x[2]), weapon_type_df.values))
+    cursor.executemany(command, data)
+    connect.commit()
