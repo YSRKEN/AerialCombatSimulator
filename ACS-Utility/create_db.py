5a05ba0b379cab84bfc6a5ee3358cba9cc07d2fa
YSRKEN <ysr.ken@gmail.com>
2018-10-21 20:15:17 +0900

    装備種テーブルと装備テーブルの作成ルーチンを一応書いた

--- ACS-Utility/create_db.py
+++ ACS-Utility/create_db.py
@@ -4,6 +4,7 @@ import os
 import pandas
 import sqlite3
 from contextlib import closing
+from typing import List
 
 def has_table(cursor, table_name: str) -> bool:
     """{table_name}テーブルの存在を確認する。あればTrueを返す
@@ -32,6 +33,49 @@ def create_weapon_type_table(cursor) -> None:
     cursor.executemany(command, data)
     connect.commit()
 
+    # 装備テーブルにインデックスを設定する
+    command = 'CREATE INDEX weapon_type_name on weapon_type(name)'
+    cursor.execute(command)
+    command = 'CREATE INDEX weapon_type_short_name on weapon_type(short_name)'
+    cursor.execute(command)
+
+def crawl_weapon_data() -> List[any]:
+    # スタブ
+    return [
+        (  0,  0, 'なし',         0, 0, 0),
+        (  2,  1, '12.7cm連装砲', 0, 0, 0),
+        ( 22,  7, '烈風',        10, 0, 0),
+        ( 54, 13, '彩雲',         0, 2, 0),
+        (168, 31, '九六式陸攻',    1, 0, 0),
+        (176, 33, '三式戦 飛燕',   9, 0, 3),
+    ]
+
+def create_weapon_table(cursor) -> None:
+    """ 装備テーブルを作成する
+    """
+    # 装備テーブルを作成する
+    if has_table(cursor, 'weapon'):
+        cursor.execute('DROP TABLE weapon')
+    command = '''CREATE TABLE [weapon] (
+                 [id] INTEGER,
+                 [type] INTEGER NOT NULL REFERENCES [weapon_type]([id]),
+                 [name] TEXT NOT NULL UNIQUE,
+                 [aa] INTEGER NOT NULL,
+                 [accuracy] INTEGER NOT NULL,
+                 [interception] INTEGER NOT NULL,
+                 PRIMARY KEY([id]))'''
+    cursor.execute(command)
+
+    # 装備テーブルにデータを追加する
+    command = 'INSERT INTO weapon (id, type, name, aa, accuracy, interception) VALUES (?,?,?,?,?,?)'
+    data = crawl_weapon_data()
+    cursor.executemany(command, data)
+    connect.commit()
+
+    # 装備テーブルにインデックスを設定する
+    command = 'CREATE INDEX weapon_name on weapon(name)'
+    cursor.execute(command)
+
 # 当該Pythonファイルのディレクトリ
 ROOT_DIRECTORY = os.path.dirname(__file__)
 
@@ -44,3 +88,6 @@ with closing(sqlite3.connect(os.path.join(ROOT_DIRECTORY, DB_PATH))) as connect:
 
     # 装備種テーブルを作成する
     create_weapon_type_table(cursor)
+
+    # 装備テーブルを作成する
+    create_weapon_table(cursor)
