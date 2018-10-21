e9b9ab3a03d09c14ae33862fa8de5e9d910bef9b
YSRKEN <ysr.ken@gmail.com>
2018-10-22 00:23:14 +0900

    電探回り以外はID・名前・装備種対応をおおむね完了

--- ACS-Utility/create_db.py
+++ ACS-Utility/create_db.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 
 import os
+import re
 import pandas
 import sqlite3
+import urllib.request
 from contextlib import closing
 from typing import List
+from bs4 import BeautifulSoup
+from pprint import pprint
 
 def has_table(cursor, table_name: str) -> bool:
     """{table_name}テーブルの存在を確認する。あればTrueを返す
@@ -39,8 +43,96 @@ def create_weapon_type_table(cursor) -> None:
     command = 'CREATE INDEX weapon_type_short_name on weapon_type(short_name)'
     cursor.execute(command)
 
+def crawl_friend_weapon_data() -> List[any]:
+    """艦娘の装備一覧をクロールして作成する
+    """
+    # 戦闘行動半径を読み込んでおく
+    weapon_radius_df = pandas.read_csv(os.path.join(ROOT_DIRECTORY, 'weapon_radius.csv'))
+    weapon_radius_dict: Dict[str, int] = {}
+    for pair in weapon_radius_df.values:
+        weapon_radius_dict[pair[0]] = pair[1]
+
+    # 装備種一覧を読み込んでおく
+    weapon_type_df = pandas.read_csv(os.path.join(ROOT_DIRECTORY, 'weapon_type.csv'))
+    weapon_type_dict: Dict[str, int] = {}
+    weapon_type_dict2: Dict[str, int] = {}
+    weapon_type_dict3: Dict[int, str] = {}
+    for pair in weapon_type_df.values:
+        if pair[3] not in weapon_type_dict:
+            weapon_type_dict[pair[3]] = pair[0]
+        weapon_type_dict2[pair[1]] = pair[0]
+        weapon_type_dict3[pair[0]] = pair[1]
+    #微調整
+    weapon_type_dict['Autogyro'] = weapon_type_dict2['対潜哨戒機']
+    weapon_type_dict['Extra Armor (Large)'] = weapon_type_dict2['増設バルジ']
+    weapon_radius_dict['Midget Submarine'] = weapon_type_dict2['艦載艇']
+    weapon_type_dict3[0] = 'なし'
+
+    # Webページを取得してパースする
+    weapon_data = [(0,0,'なし',0,0,0)]
+    with urllib.request.urlopen('http://kancolle.wikia.com/wiki/Equipment') as request:
+        # 取得、パース
+        soup: BeautifulSoup = BeautifulSoup(request.read(), 'html.parser')
+        trTagList = soup.select("table.wikitable tr")
+
+        # 1行づつ読み取っていく
+        for trTag in trTagList:
+            # 関係ない行は無視する
+            tdTagList = trTag.select("td")
+            if len(tdTagList) < 9:
+                continue
+
+            # 装備IDを読み取る
+            id = int(tdTagList[0].text)
+
+            # 装備名を読み取る
+            name = tdTagList[2].text.replace(tdTagList[2].a.text, '', 1)
+            name = re.sub('(^ |\n)', '', name)
+
+            # 装備種を読み取る
+            weapon_type = re.sub("\n", '', tdTagList[3].text)
+            #辞書による自動判断
+            if weapon_type in weapon_type_dict:
+                weapon_type = weapon_type_dict[weapon_type]
+            else:
+                if 'Radar' in weapon_type:
+                    weapon_type = weapon_type_dict2['水上電探']
+                else:
+                    weapon_type = weapon_type_dict2['その他']
+            #個別対処
+            if weapon_type == weapon_type_dict2['艦上偵察機'] and '彩雲' in name:
+                weapon_type = weapon_type_dict2['艦上偵察機(彩雲)']
+            if weapon_type == weapon_type_dict2['艦上爆撃機'] and '爆戦' in name:
+                weapon_type = weapon_type_dict2['艦上爆撃機(爆戦)']
+            if weapon_type == weapon_type_dict2['爆雷投射機'] and '投射機' not in name:
+                weapon_type = weapon_type_dict2['爆雷']
+            if weapon_type == weapon_type_dict2['局地戦闘機'] and name not in ['雷電', '紫電一一型', '紫電二一型 紫電改', '紫電改(三四三空) 戦闘301']:
+                weapon_type = weapon_type_dict2['陸軍戦闘機']
+            #水上電探・対空電探は個別対処
+
+            print("[" + str(id) + "] " + name + " " + weapon_type_dict3[weapon_type])
+    return []
+
+def crawl_enemy_weapon_data() -> List[any]:
+    """深海棲艦の装備一覧をクロールして作成する
+    """
+    return []
+
 def crawl_weapon_data() -> List[any]:
-    # スタブ
+    """装備一覧をWebクロールして作成する
+    """
+    # 艦娘の装備一覧を読み取る
+    friend_weapon_data = crawl_friend_weapon_data()
+
+    # 深海棲艦の装備一覧を読み取る
+    enemy_weapon_data = crawl_enemy_weapon_data()
+
+    # 合体させたものを戻り値とする
+    weapon_data = friend_weapon_data
+    weapon_data.extend(enemy_weapon_data)
+    return weapon_data
+
+    """
     return [
         (  0,  0, 'なし',         0, 0, 0),
         (  2,  1, '12.7cm連装砲', 0, 0, 0),
@@ -49,6 +141,7 @@ def crawl_weapon_data() -> List[any]:
         (168, 31, '九六式陸攻',    1, 0, 0),
         (176, 33, '三式戦 飛燕',   9, 0, 3),
     ]
+    """
 
 def create_weapon_table(cursor) -> None:
     """ 装備テーブルを作成する
