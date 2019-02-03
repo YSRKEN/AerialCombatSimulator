c8c35a1f1fc91cbab2f3ba3433eea082e6403c70
YSRKEN <ysr.ken@gmail.com>
2019-02-03 12:57:50 +0900

    基地航空隊の計算結果を表示することに成功した

--- ACS-Frontend_/src/app/form/simulation-result/simulation-result.component.ts
+++ ACS-Frontend_/src/app/form/simulation-result/simulation-result.component.ts
@@ -36,6 +36,7 @@ export class SimulationResultComponent implements OnInit {
         backgroundColor: "rgba(160,194,56,0.4)",
         pointBorderColor: "rgba(160,194,56,1)",
         data: [],
+        yAxisID: 'y-right'
       },{
         label: '下側確率',
         type: "line",
@@ -66,8 +67,15 @@ export class SimulationResultComponent implements OnInit {
           }],
           yAxes: [{
             display: true,
-            scaleLabel: { display: true, labelString: '割合(％)' },
+            scaleLabel: { display: true, labelString: '下側確率の割合(％)' },
             ticks: { min: 0, max: 100, stepSize: 10 }
+          },
+          {
+            display: true,
+            scaleLabel: { display: true, labelString: '確率分布の割合(％)' },
+            ticks: { min: 0 },
+            id: 'y-right',
+            position: 'right'
           }]
         }
       }
@@ -178,9 +186,48 @@ export class SimulationResultComponent implements OnInit {
    * シミュレーションを開始する
    */
   async startSimulation(){
+    // 情報を取得する
     const lbas = this.getLBASData();
     const enemy = this.getEnemyData();
     const own = this.getOwnData();
-    console.log(await this.restApi.postSimulation(lbas, enemy, own, this.SimulationType));
+
+    // サーバーにクエリを投げる
+    const simulationResult = await this.restApi.postSimulation(lbas, enemy, own, this.SimulationType);
+
+    // サーバーから返ってきた結果を処理する
+    console.log(simulationResult);
+    const enemyAntiAirValueDict: { [key: number]: number; } = simulationResult['EnemyAntiAirValue'];
+    this.redrawEnemyAntiAirValue(enemyAntiAirValueDict);
+  }
+
+  /**
+   * 敵制空値の連想配列から、グラフ描画用のデータに変換する
+   * @param enemyAntiAirValueDict 敵制空値の連想配列。dict[制空値]=出現回数
+   */
+  redrawEnemyAntiAirValue( enemyAntiAirValueDict: { [key: number]: number; }) {
+    // 反復回数の合計値を出す
+    var sum = 0;
+    for (var aav in enemyAntiAirValueDict) {
+      var count = enemyAntiAirValueDict[aav];
+      sum += count;
+    }
+
+    // 割合の表と累積の表を両方作成する
+    var graph1: {x: number, y: number}[] = [];
+    var graph2: {x: number, y: number}[] = [];
+    var sum2 = 0;
+    for (var aav in enemyAntiAirValueDict) {
+      var count = enemyAntiAirValueDict[aav];
+      graph1.push({x: parseInt(aav), y: 100.0 * count / sum});
+      sum2 += count;
+      graph2.push({x: parseInt(aav), y: 100.0 * sum2 / sum});
+    }
+
+    // グラフに計算結果をセットする
+    console.log(graph1)
+    console.log(graph2)
+    this.data.datasets[0].data = graph1
+    this.data.datasets[1].data = graph2
+    this.chart.update()
   }
 }
